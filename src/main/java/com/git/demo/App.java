414bc49c3caa0b9974231e7f6baa3e3eb361f4c7
unknown <mohd.khurseed@gmail.com>
2024-08-02 20:53:52 +0800

    Modified to add if statement

--- src/main/java/com/git/demo/App.java
+++ src/main/java/com/git/demo/App.java
@@ -8,6 +8,9 @@ public class App
 {
 	public void addData(String data) {
 		System.out.println("Data "+data);
+		if(data.equalsIgnoreCase(Constant.DATA)) {
+			System.out.println("Welcome");
+		}
 	}
     public static void main( String[] args )
     {
---
+++ src/main/java/com/git/demo/Constant.java
@@ -0,0 +1,7 @@
+package com.git.demo;
+
+public class Constant {
+	
+	public static String DATA = "Noamzomu";
+
+}
